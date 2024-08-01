e2ffce9ecea8be5b379e30e868a6ab2adec14769
bw007 <abdulaziz00791@gmail.com>
2024-08-02 01:35:58 +0500

    Student view

--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-    <h1>{{ $route.name }}</h1>
+  <div class="flex flex-col gap-2">
+    <div class="flex items-center justify-between">
+      <h1 class="font-bold text-xl">Student List</h1>
+      <button class="bg-amber-400 hover:bg-amber-500 active:bg-amber-400 text-white font-medium text-sm py-2 px-4 rounded">
+        ADD NEW STUDENT
+      </button>
+    </div>
+    <StudentList />
   </div>
 </template>
 
 <script>
+import StudentList from '@/components/students/StudentList.vue';
+
 export default {
-  
+  components: {
+    StudentList
+  }
 }
 </script>
