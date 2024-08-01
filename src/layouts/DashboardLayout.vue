796b46b98000660df4501a549f8c7504921956b7
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-01 11:34:11 +0500

    Full layout completed

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="flex h-screen">
     <SidebarPart />
-    <HeaderPart />
+    <div class="flex-1 flex flex-col">
+      <HeaderPart />
+      <MainPart />
+    </div>
   </div>
 </template>
 <script>
 import SidebarPart from "@/components/dashboard/SidebarPart.vue";
 import HeaderPart from "@/components/dashboard/HeaderPart.vue";
+import MainPart from "@/components/dashboard/MainPart.vue";
 export default {
   components: {
     SidebarPart,
-    HeaderPart
+    HeaderPart,
+    MainPart
   }
 }
 </script>
