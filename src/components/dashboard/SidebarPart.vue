7f5f34dae15ec8d474a9252125fd17236a76470f
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-01 12:38:02 +0500

    Sidebar component

--- src/components/dashboard/SidebarPart.vue
+++ src/components/dashboard/SidebarPart.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="bg-[#F2EAE1] w-64 flex-shrink-0">
+  <aside class="bg-[#F2EAE1] w-64 flex-shrink-0">
     <div class="p-4">
       <h1 class="border-l-4 border-l-[#F8D442] uppercase leading-none px-2 text-2xl font-bold">Crud</h1>
     </div>
     <nav class="p-4">
-      <ul class="flex flex-col gap-3">
-        <li><a href="#" class="block p-2 rounded bg-[#FEAF00]">Home</a></li>
-        <li><a href="#" class="block p-2 rounded hover:bg-[#FEAF0030]">Profile</a></li>
-        <li><a href="#" class="block p-2 rounded hover:bg-[#FEAF0030]">Settings</a></li>
-        <li><a href="#" class="block p-2 rounded hover:bg-[#FEAF0030]">Home</a></li>
-        <li><a href="#" class="block p-2 rounded hover:bg-[#FEAF0030]">Profile</a></li>
-        <li><a href="#" class="block p-2 rounded hover:bg-[#FEAF0030]">Settings</a></li>
+      <ul class="flex flex-col gap-2 font-medium text-sm">
+        <li v-for="item in menu" :key="item.name">
+            <router-link
+              :to="{name: item.name}"
+              href="#" 
+              class="flex items-center gap-1 px-3 py-2 rounded hover:bg-[#FEAF0030]" 
+              :class="{'bg-[#FEAF00!important]': $route.name == item.name}"
+            >
+               <component :size="18" :is="item.icon" />
+              {{ item.title }}
+            </router-link>
+        </li>
       </ul>
     </nav>
-  </div>
+  </aside>
 </template>
 
 <script>
-export default {
+import { menu } from '@/stores/menu';
 
+export default {
+  components: {},
+  data() {
+    return {
+      menu
+    }
+  },
 }
 </script>
