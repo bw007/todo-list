6925280b552a9fb875c6efb85e8729c00ede506d
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-01 11:46:18 +0500

    Other routes

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -1,10 +1,27 @@
+import { GraduationCap, LibraryBig, PieChart } from "lucide-vue-next";
 
 
 export const menu = [
   {
     path: "",
     name: "Home",
+    title: "Home",
     component: () => import("@/views/HomeView.vue"),
-    icons: ""
-  }
+    icon: PieChart
+  },
+  {
+    path: "course",
+    name: "Course",
+    title: "Course",
+    component: () => import("@/views/CourseView.vue"),
+    icon: LibraryBig
+  },
+  
+  {
+    path: "students",
+    name: "Students",
+    title: "Students",
+    component: () => import("@/views/CourseView.vue"),
+    icon: GraduationCap
+  },
 ]
