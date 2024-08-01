faafb47bc432afd6c21e354799f5424c824d5aa6
bw007 <abdulaziz00791@gmail.com>
2024-08-02 00:31:15 +0500

    Update

--- src/components/dashboard/HeaderPart.vue
+++ src/components/dashboard/HeaderPart.vue
@@ -3,7 +3,7 @@ import { User2 } from 'lucide-vue-next';
 </script>
 
 <template>
-  <div class="bg-gray-100 py-3 px-6 shadow">
+  <div class="bg-gray-50 py-3 px-6 shadow-sm">
     <div class="flex justify-end items-center">
       <div class="cursor-pointer">
         <User2 :size="32" class="inline-block p-2 rounded-full ring-2 ring-white" />
