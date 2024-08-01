882d2c1e59b93f409541288b7134efef75d11446
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-01 11:33:09 +0500

    Config fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      'montserrat': 'Montserrat'
+    }
   },
   plugins: []
 }
