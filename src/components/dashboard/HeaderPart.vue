f8226bddb2b9879d3b48e668c12d1f63e16eca3a
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-01 12:38:13 +0500

    Header component

--- src/components/dashboard/HeaderPart.vue
+++ src/components/dashboard/HeaderPart.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="flex-1 flex flex-col">
-    <!-- Navbar -->
-    <div class="bg-gray-100 p-4 shadow">
-      <div class="flex justify-between items-center">
-        <h1 class="text-xl font-semibold">Welcome</h1>
-        <div>
-          <button class="bg-blue-500 text-white px-4 py-2 rounded">Logout</button>
+  <div class="bg-gray-100 py-3 px-6 shadow">
+    <!-- <div class="flex justify-between items-center">
+      <div>
+        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+      </div>
+      
+    </div> -->
+    <div class="relative">
+      <button type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
+        aria-expanded="false">
+        <span>Solutions</span>
+        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clip-rule="evenodd" />
+        </svg>
+      </button>
+
+      <!--
+        Flyout menu, show/hide based on flyout menu state.
+
+        Entering: "transition ease-out duration-200"
+          From: "opacity-0 translate-y-1"
+          To: "opacity-100 translate-y-0"
+        Leaving: "transition ease-in duration-150"
+          From: "opacity-100 translate-y-0"
+          To: "opacity-0 translate-y-1"
+      -->
+      <div class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+        <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+          <a href="#" class="font-semibold text-gray-900">
+                Automations
+                <span class="absolute inset-0"></span>
+              </a>
         </div>
       </div>
-    </div>
 
-    <!-- Main Content Area -->
-    <div class="flex-1 p-4 overflow-auto bg-white">
-      <h2 class="text-2xl font-bold mb-4">Dashboard Content</h2>
-      <p>Here is the main content of the dashboard...</p>
     </div>
   </div>
 </template>
