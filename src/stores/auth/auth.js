2f759a988809d4f9ed5159e0635c63defe286071
bw007 <abdulaziz00791@gmail.com>
2024-08-03 06:37:59 +0500

    user data

--- src/stores/auth/auth.js
+++ src/stores/auth/auth.js
@@ -5,7 +5,14 @@ export const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
-    console.log('Foydalanuvchi kirishi:', user);
+
+    const userInfo = {
+      uid: user.uid,
+      displayName: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL
+    };
+    console.log('Foydalanuvchi kirishi:', userInfo);
     return user;
   } catch (error) {
     console.error('Google bilan kirishda xatolik:', error);
