98e2f0d844178377c2963fc22ffcf83cbca9ff06
bw007 <abdulaziz00791@gmail.com>
2024-08-03 06:37:49 +0500

    get from env file

--- src/stores/firebase.js
+++ src/stores/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyjfyyRS3vNqg8zd93ux6rKdE7ZeqLqLo",
-  authDomain: "vue-crud-97a6e.firebaseapp.com",
-  projectId: "vue-crud-97a6e",
-  storageBucket: "vue-crud-97a6e.appspot.com",
-  messagingSenderId: "705448281967",
-  appId: "1:705448281967:web:74e319d628b95102390a9a",
-  measurementId: "G-RRJL1HBP6M"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
