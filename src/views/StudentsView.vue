e92a5731207e248f1bd5ab9e1f7bd56fd8094f49
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-05 17:18:41 +0500

    Form inputs

--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -7,12 +7,30 @@
       </button>
       <DialogPart
         :isVisible="showDialog" 
-        title="Tasdiqlash dialogi"
+        title="New student"
+        confirmButtonText="Yo'q"
         @close="showDialog = false"
         @cancel="handleCancel"
         @confirm="handleConfirm"
       >
-        <p>Bu dialog oynasida matn bor.</p>
+        <form @submit.prevent="handleSubmit">
+          <div class="mb-4">
+            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+            <input id="name" v-model="student.name" type="text" required class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+            <input id="email" v-model="student.email" type="email" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
+          </div>
+          <div class="mb-4">
+            <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
+            <input id="phone" v-model="student.phone" type="tel" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
+          </div>
+          <div class="mb-4">
+            <label for="date" class="block text-sm font-medium text-gray-700">Date</label>
+            <input id="date" v-model="student.date" type="date" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
+          </div>
+        </form>
       </DialogPart>
     </div>
     <StudentList />
@@ -40,7 +58,13 @@ export default {
   },
   data() {
     return {
-      showDialog: false
+      showDialog: false,
+      student: {
+        name: '',
+        email: '',
+        phone: '',
+        date: ''
+      }
     }
   },
 }
