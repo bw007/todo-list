1e12b9f30ac1e0e6f740ea5a72af02cadbf12bd6
bw007 <abdulaziz00791@gmail.com>
2024-08-06 07:18:20 +0500

    Add libs

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
+import VueTheMask from 'vue-the-mask';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { unstyled: true })
+app.use(VueTheMask)
 
 app.mount('#app')
