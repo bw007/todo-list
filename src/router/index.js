a651e1c729cb27d6a94eb35313693c890e537bf0
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-07 17:22:59 +0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
-import { menu } from '@/stores/menu'
+import { menu } from '@/stores/utils/menu'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
