414b59802662d6eaa5619ebbc40c46d6c9c404b1
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-07 17:26:37 +0500

    Test vee validate

--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -8,26 +8,27 @@
       <DialogPart
         :isVisible="showDialog" 
         title="New student"
-        confirmButtonText="Ha"
-        cancelButtonText="Yo'q"
         @close="showDialog = false"
-        @cancel="handleCancel"
-        @confirm="handleConfirm"
       >
-        <form @submit.prevent="handleSubmit" class="mt-4">
+        <Form @submit="handleSubmit" class="mt-4">
           <div class="mb-3">
             <label for="name" class="block text-xs mb-[2px] font-medium text-gray-700">Name</label>
-            <input id="name" v-model="student.name" placeholder="Name" type="text" required class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <Field name="name" placeholder="Name" type="text" required v-model="student.name" class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
           <div class="mb-3">
-            <label for="name" class="block text-xs mb-[2px] font-medium text-gray-700">Email</label>
-            <input id="name" v-model="student.email" placeholder="Email" type="email" required class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <label for="email" class="block text-xs mb-[2px] font-medium text-gray-700">Email</label>
+            <Field name="email" :rules="validateEmail" placeholder="someone@example.com" type="email" required v-model="student.email" class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <ErrorMessage name="email" />
           </div>
           <div class="mb-3">
-            <label for="name" class="block text-xs mb-[2px] font-medium text-gray-700">Phone</label>
-            <input id="phone" v-model="student.phone" v-mask="'+998 (##) ###-##-##'" placeholder="+998 (__) ___-__-__" required class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <label for="email" class="block text-xs mb-[2px] font-medium text-gray-700">Phone</label>
+            <Field name="phone" placeholder="+998 (__) ___-__-__" v-mask="'+998 (##) ###-##-##'" required v-model="student.phone" class="bg-transparent border outline-none border-gray-300 text-gray-900 text-sm rounded focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
-        </form>
+          <div class="actions mt-4 flex items-center justify-end gap-3">
+            <button class="border text-sm font-medium hover:text-amber-500 hover:bg-amber-50 rounded w-20 p-1" @click="cancel">Yo'q</button>
+            <button class="border border-amber-500 bg-amber-500 hover:opacity-80 text-white text-sm font-medium rounded w-20 p-1" @click="confirm">ha</button>
+          </div>
+        </Form>
       </DialogPart>
     </div>
     <StudentList />
@@ -37,11 +38,15 @@
 <script>
 import DialogPart from '@/components/ui/DialogPart.vue';
 import StudentList from '@/components/students/StudentList.vue';
+import { Form, Field, ErrorMessage } from 'vee-validate';
 
 export default {
   components: {
     StudentList,
     DialogPart,
+    Form,
+    Field,
+    ErrorMessage
   },
   methods: {
     handleCancel() {
@@ -53,7 +58,23 @@ export default {
       console.log('Tasdiqlandi');
       this.showDialog = false;
       this.student = {}
-    }
+    },
+    handleSubmit(values) {
+      console.log(values);
+    },
+    validateEmail(value) {
+      // if the field is empty
+      if (!value) {
+        return 'This field is required';
+      }
+      // if the field is not a valid email
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'This field must be a valid email';
+      }
+      // All is good
+      return true;
+    },
   },
   data() {
     return {
@@ -68,7 +89,3 @@ export default {
   },
 }
 </script>
-
-<style>
-
-</style>
