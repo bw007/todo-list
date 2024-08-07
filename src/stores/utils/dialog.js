241a77c3fe22e66dd0f1706958ed3e5cfb935e93
bw007 <abdulaziz00791@gmail.com>
2024-08-07 22:30:49 +0500

    fix

--- src/stores/utils/dialog.js
+++ src/stores/utils/dialog.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useDialogStore = defineStore('dialogStore', {
   state: () => ({
-    toggle: true,
+    toggle: false,
   }),
   actions: {
     setToggle(value) {
